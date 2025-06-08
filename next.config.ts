08e5c370dbc7181ad4865958a25efbc4ec07cf7a
HLDDian <a59715a@yahoo.com.tw>
2025-06-08 21:33:31 +0800

    更新 next.config.ts 檔案以符合專案需求

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: "/project-goal-sample",
-  assetPrefix: "/project-goal-sample",
   /* config options here */
 };
 
